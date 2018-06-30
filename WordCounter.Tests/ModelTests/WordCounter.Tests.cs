626c58773c20e66e09430720f25641720a3c5cc1
Derek Hammer <derekhammer@hotmail.com>
2018-06-29 17:19:38 -0700

    Final commit with all logic and user interface

--- WordCounter.Tests/ModelTests/WordCounter.Tests.cs
+++ WordCounter.Tests/ModelTests/WordCounter.Tests.cs
@@ -8,6 +8,7 @@ namespace WordChecker.TestTools
   [TestClass]
   public class WordCounterTest
   {
+    //Tests if I can input a word
     [TestMethod]
     public void UserWord_GetWord_True()
     {
@@ -20,7 +21,7 @@ namespace WordChecker.TestTools
       //Assert
       Assert.AreEqual(result, newWord);
     }
-
+    //Tests if I can input a paragraph.  Essentially the same as the test above
     [TestMethod]
     public void UserParagraph_GetParagraph_True()
     {
@@ -34,21 +35,39 @@ namespace WordChecker.TestTools
       Assert.AreEqual(result, newParagraph);
       Console.WriteLine(result);
     }
+    //Tests if I can split my input paragraph into an array of words!
     [TestMethod]
     public void UserParagraph_SplitParagraph_True()
     {
+      //Arrange
       string newParagraph = "oh boy, i sure love pizza";
       string[] array = {"oh", "boy,", "i", "sure", "love", "pizza"};
       words newWords = new words();
-
+      //Act
       newWords.SetParagraph(newParagraph);
       newWords.GetParagraph();
       string[] result = newWords.Split();
-
+      //Assert
       CollectionAssert.AreEqual(array, result);
       Console.WriteLine(result);
     }
-
+    //Compares the input word against the input paragraph and spits out a value
+    [TestMethod]
+    public void UserParagraph_Compare_True()
+    {
+      //Arrange
+      string newWord = "pizza";
+      string newParagraph = "oh boy, i sure love pizza";
+      words newWords = new words();
+      //Act
+      newWords.SetWord(newWord);
+      newWords.SetParagraph(newParagraph);
+      string testWord = newWords.GetWord();
+      string[] testPara = newWords.Split();
+      int result = newWords.Compare(testWord, testPara);
+      //Assert
+      Assert.AreEqual(result, 1);
+    }
 
 
   }
--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -7,32 +7,64 @@ namespace WordCounter
     {
       private string _userWord;
       private string _userParagraph;
+      private int _score;
       //private static int _wordCount;
 
-      //Sets the word to be checked
+      //Sets the word
       public void SetWord (string UserWord)
       {
         _userWord = UserWord;
       }
-      //Gets the word to ge checked
+      //Gets the word
       public string GetWord ()
       {
         return _userWord;
       }
-
+      //Sets the paragraph
       public void SetParagraph (string UserParagraph)
       {
         _userParagraph = UserParagraph;
       }
-      //Gets the word to ge checked
+      //Gets the paragraph
       public string GetParagraph ()
       {
         return _userParagraph;
       }
+      //splits the user paragraph into an array of words
       public string[] Split ()
       {
         return _userParagraph.Split(' ');
       }
+      //compares the array paragraph against the input word
+      public int Compare (string inputWord, string[] inputParagraph)
+      {
+        int wordCount =  0;
+            foreach(string word in inputParagraph)
+            {
+                if(word == inputWord)
+                {
+                    wordCount++;
+                }
+            }
+            _score = wordCount;
+            return _score;
+      }
     }
-
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Type in a word you want to check for.");
+      string typedWord = Console.ReadLine();
+      Console.WriteLine("Type in a paragraph that we can check for that word.");
+      string typedParagraph = Console.ReadLine();
+      words newWords = new words();
+      newWords.SetWord(typedWord);
+      newWords.SetParagraph(typedParagraph);
+      string testWord = newWords.GetWord();
+      string[] testPara = newWords.Split();
+      int result = newWords.Compare(testWord, testPara);
+      Console.WriteLine("Your word appears " + result + " time(s).");
+    }
+  }
 }
